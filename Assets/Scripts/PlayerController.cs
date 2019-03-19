ff374318e95c43c0691bd1aa41cd2e46e3e3fb4a
Denys Zosym <mrpunke@gmail.com>
2019-03-19 22:03:43 +0100

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private float moveSpeedStore;
+    public float jumpForce;
+
+    public float speedMultiplier;
+    public float speedIncreaseMilestone;
+    private float speedIncreaseMilestoneStore;
+
+    private float speedMilestoneCount;
+    private float speedMilestoneCountStore;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
+
+    private bool canDoubleJump;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    //private Collider2D myCollider;
+
+    private Rigidbody2D myRigidBody;
+
+    private Animator myAnimator;
+    public GameManager theGameManager;
+
+    public AudioSource jumpSound;
+    public AudioSource deathSound;
+
+	// Use this for initialization
+	void Start () {
+        myAnimator = GetComponent<Animator>();
+
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        //myCollider = GetComponent<Collider2D>();
+
+        jumpTimeCounter = jumpTime;
+
+        speedMilestoneCount = speedIncreaseMilestone;
+
+        moveSpeedStore = moveSpeed;
+        speedMilestoneCountStore = speedMilestoneCount;
+        speedIncreaseMilestoneStore = speedIncreaseMilestone;
+
+        stoppedJumping = true;  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (transform.position.x > speedMilestoneCount)
+        {
+            speedMilestoneCount += speedIncreaseMilestone;
+
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
+            moveSpeed = moveSpeed * speedMultiplier;
+        } 
+
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) //проверка на нажатую кнопку
+        {
+            if (grounded)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+                stoppedJumping = false;
+                jumpSound.Play();
+            }
+            if (!grounded && canDoubleJump)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+                jumpTimeCounter = jumpTime;
+                stoppedJumping = false;
+                canDoubleJump = false;
+                jumpSound.Play();
+            } 
+        }
+        if ((Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) && !stoppedJumping)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+            
+        } 
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+            canDoubleJump = true;
+        }
+
+        myAnimator.SetFloat("Speed", myRigidBody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
+	}
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.tag == "killbox")
+        {
+            theGameManager.RestartGame();
+            moveSpeed = moveSpeedStore;
+            speedMilestoneCount = speedMilestoneCountStore;
+            speedIncreaseMilestone = speedIncreaseMilestoneStore;
+            deathSound.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/pickupPoints.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupPoints : MonoBehaviour {
+
+    public int scoreToGive;
+    private ScoreManager theScoreManager;
+
+    private AudioSource coinSound;
+
+	// Use this for initialization
+	void Start () {
+        theScoreManager = FindObjectOfType<ScoreManager>();
+
+        coinSound = GameObject.Find("CoinSound").GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D (Collider2D other) //ontriggerenter2d - проверяет "прошло" ли что-то через объект(монета); (Collider2D other) - объект который прошёл
+    {
+        if (other.gameObject.name == "Player")
+        {
+            theScoreManager.AddScore(scoreToGive);
+            gameObject.SetActive(false);
+
+            if (coinSound.isPlaying)
+            {
+                coinSound.Stop();
+                coinSound.Play();
+            }
+            else
+            {
+                coinSound.Play();
+            }
+
+            
+        }
+    }
+}
